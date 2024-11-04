782dfad1f1d9baf1889f3b69e6b882e5daa85d40
Ahelios <artursmedveds@gmail.com>
2024-11-04 17:21:01 +0200

    Initial commit

---
+++ 1c/1c/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+function Header({course}) {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+function Content({parts}){
+  console.log(parts);
+  return (
+    <>
+    <Part part={parts[0].part} numberOfExcersises={parts[0].excercises}/>
+    <Part part={parts[1].part} numberOfExcersises={parts[1].excercises}/>
+    <Part part={parts[2].part} numberOfExcersises={parts[2].excercises}/>
+    </>
+  )
+}
+
+function Part({part, numberOfExcersises}){
+  return(
+    <p>
+			{part} {numberOfExcersises}
+		</p>
+  )
+}
+
+function Total({excercises}){
+  const sum = excercises.reduce((acc, curr) => acc + curr, 0);
+
+  return(
+    <p>Number of excercises ${sum}</p>
+  )
+}
+
+function App() {
+	const course = 'Half Stack application development';
+
+  const parts = [
+    {part:'Fundamentals of React', excercises: 10},
+    {part:'Using props to pass data', excercises: 7},
+    {part:'State of a component', excercises: 14}
+  ]
+
+	// const part1 = 'Fundamentals of React';
+	// const exercises1 = 10;
+	// const part2 = 'Using props to pass data';
+	// const exercises2 = 7;
+	// const part3 = 'State of a component';
+	// const exercises3 = 14;
+
+	return (
+		<>
+			<div>
+        <Header course={course}/>
+				<Content parts={parts}/>
+				<Total excercises={[parts[0].excercises, parts[1].excercises, parts[2].excercises]}/>
+			</div>
+		</>
+	);
+}
+
+export default App;
